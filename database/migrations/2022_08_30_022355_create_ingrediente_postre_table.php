705fd8ecc2b6ae367d12b5189e36d57ebd7a3617
henrrycasa <githycc@gmail.com>
2022-08-31 16:54:54 -0300

    modificacion_tablas

--- database/migrations/2022_08_30_022355_create_ingrediente_postre_table.php
+++ database/migrations/2022_08_30_022355_create_ingrediente_postre_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('ingrediente_postre', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('id_postres')
+            ->constrained('postres')
+            ->cascadeOnUpdate();
         });
     }
 
